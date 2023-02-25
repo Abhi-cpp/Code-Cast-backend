de6cb488f3c4199b4c780145e7c243de43310bd7
Abhishek Kumar <Abhishekkumar986874@gmail.com>
2023-02-25 13:52:17 +0530

    changes to host in online

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
     cors: {
-        origin: process.env.FRONTEND_URL,
+        origin: "*",
     }
 });
 
@@ -35,7 +35,9 @@ io.on("connect", onConnection);
 app.use(cors());
 app.use(express.json());
 app.use(userRouter);
-
+app.use('/', (req, res) => {
+    res.send("server is up and running")
+})
 
 
 DBConnect().then(() => {
